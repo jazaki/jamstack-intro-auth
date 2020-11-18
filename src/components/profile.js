2f319bf649ebb74e209dcdb21c692bc271481ad1
Junaid Arham <jarham@chegg.com>
2020-11-18 16:39:29 +0530

    feat: protecting private routes

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,25 +1,36 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import { useIdentityContext } from 'react-netlify-identity'
 
-function Profile() {
+function Profile({ showModal }) {
+  const identity = useIdentityContext()
+  const isLoggedIn = identity && identity.isLoggedIn
+  const name = identity && identity.user && identity.user.user_metadata && identity.user.user_metadata.full_name
   return (
-    <div className="dashboard-header">
-      <nav>
-        <Link
-          to="/dashboard/secret"
-          activeClassName="active"
-        >
-          Secret Stuff
-        </Link>
-        <Link
-          to="/dashboard/base"
-          activeClassName="active"
-        >
-          See your base
-        </Link>
-      </nav>
-      <span>TODO show login status</span>
-    </div>
+    isLoggedIn && (
+      <div className="dashboard-header">
+        <nav>
+          <Link
+            to="/dashboard/secret"
+            activeClassName="active"
+          >
+            Secret Stuff
+          </Link>
+          <Link
+            to="/dashboard/base"
+            activeClassName="active"
+          >
+            See your base
+          </Link>
+        </nav>
+        <span onClick={ showModal }>
+          Logged in as {name}
+          <button>
+            Logout
+          </button>
+        </span>
+      </div>
+    )
   )
 }
 
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -5,6 +5,7 @@ import { IdentityModal } from 'react-netlify-identity-widget'
 
 import Layout from '../components/layout'
 import Profile from '../components/profile'
+import PrivateRoute from '../components/private-routes'
 import RouteBase from '../components/route-base'
 import RouteSecret from '../components/route-secret'
 import RouteLogin from '../components/route-login'
@@ -23,14 +24,13 @@ function Dashboard({ location }) {
 
   return (
     <Layout>
-      <Profile />
+      <Profile showModal={showModal} />
       <Router>
-        <RouteBase path="/dashboard/base" />
-        <RouteSecret path="/dashboard/secret" />
-        <RouteLogin path="/dashboard/login" showModal={showModal} />
+        <PrivateRoute path="/dashboard/base" component={RouteBase}/>
+        <PrivateRoute path="/dashboard/secret" component={RouteSecret} />
+        <PrivateRoute path="/dashboard/login" showModal={showModal} component={RouteLogin} />
       </Router>
       <IdentityModal
-        aria-label="jojo"
         showDialog={ isVisible }
         onCloseDialog={ () => showModal(false) }
       />
